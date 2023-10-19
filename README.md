30fdc8140d795c9ee0f79791097a9614bdd8f057
Black3800 <black38000@gmail.com>
2023-10-19 16:39:38 +0700

    add: post rgb/cct

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # DuckDuck Illumination Service
 
 ## Install
+
 ```bash
-docker run -dp 8080:8080
+docker build . -t duckduck-illumination
+docker run -dp 8080:8080 duckduck-illumination
 ```
--- main.py
+++ main.py
@@ -2,19 +2,31 @@ from zenggebulb import ZenggeBulb
 
 from typing import Union
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 
 app = FastAPI()
+bulb = ZenggeBulb("192.168.101.201")
+
+class RGBColor(BaseModel):
+    red: int
+    green: int
+    brightness: int
+
+class CCTColor(BaseModel):
+    temp: int
+    brightness: int
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/rgb")
+def post_rgb(data: RGBColor):
+    rgb = jsonable_encoder(data)
+    bulb.set_rgb(rgb.red, rgb.green, rgb.brightness)
+    return {"status": "ok"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-# bulb = ZenggeBulb("192.168.101.201")
-# bulb.set_cct(0, 50)
+@app.post("/cct")
+def post_cct(data: CCTColor):
+    cct = jsonable_encoder(data)
+    bulb.set_cct(cct.temp, cct.brightness)
+    return {"status": "ok"}
